3342dece7c03539e96b9693d9d621fd17b850831
Diego Miño <diegominho92@gmail.com>
2023-11-30 00:12:23 -0300

    onResetForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -6,7 +6,6 @@ export const useForm = (initialForm = {}) => {
 	// función que permite realizar el cambio de cualquier input
 	const onInputChange = ({ target }) => {
 		const { name, value } = target;
-
 		setFormState({
 			// Utilizamos el spread operator para no modificar el objeto original
 			...formState,
@@ -14,10 +13,15 @@ export const useForm = (initialForm = {}) => {
 		});
 	};
 
+	const onResetForm = () => {
+		setFormState(initialForm);
+	};
+
 	return {
 		// desestructuramos el formState, para poder utilizar todas las propiedades en el Hook
 		...formState,
 		formState,
 		onInputChange,
+		onResetForm,
 	};
 };
