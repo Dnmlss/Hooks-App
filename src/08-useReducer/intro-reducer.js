3bffcb20475993f96313a4429f5dfbf7f1fc4877
Diego Miño <diegominho92@gmail.com>
2023-12-16 15:35:24 -0300

    intro-reducer

---
+++ src/08-useReducer/intro-reducer.js
@@ -0,0 +1,14 @@
+const initialState = [
+	{
+		id: 1,
+		todo: 'Recolectar la piedra del Alma',
+		done: false,
+	},
+];
+
+const todoReducer = (state = initialState, action) => {
+	return state;
+};
+
+let todos = todoReducer();
+console.log(todos);
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import ReactDOM from 'react-dom/client';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-//import { FocusScreen } from './04-useRef/FocusScreen';
-//import { Layout } from './05-useLayoutEffect/Layout';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+// import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
-//import { MemoHook } from './06-memos/MemoHook';
+// import { MemoHook } from './06-memos/MemoHook';
 // import { CallBackHook } from './06-memos/CallBackHook';
 import { Padre } from './07-tarea-memo/Padre';
+
+import './08-useReducer/intro-reducer';
 
 import './index.css';
 
